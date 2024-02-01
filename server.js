2275c211b1ac690476fbff617fe7a2679b270a3e
HiMashuguptA <sg707012@gmail.com>
2024-02-01 19:36:56 +0530

    updated deployed link

--- server.js
+++
@@ -1,9 +0,0 @@
-var express= require('express')
-const app = express()
-const port = 3120
-app.get('/ping',(req,res)=>{
-    res.send("pong")
-})
-app.listen(port,()=>{
-    console.log("port running in 3120...");
-})
