772ba56878ac9edc2b87684082dbe6c52a1a887d
HiMashuguptA <sg707012@gmail.com>
2024-02-06 11:57:30 +0530

    CRUD MEthods routes

---
+++ Backend/routes.js
@@ -0,0 +1,67 @@
+var express = require('express')
+var app = express()
+var {startServer,closeServer} = require('./server')
+var port =3350;
+const Products = require('./schema')
+const bodyParser = require('body-parser')//it is used to to collect any type of data and convert them in a readable json file
+app.use(bodyParser.json()) // converts the all recieved data in json form
+app.use(express.json())
+
+var LaptopData = [
+    { id: 1, brand: 'Dell', model: 'XPS 13' },
+    { id: 2, brand: 'HP', model: 'Spectre x360' },
+    { id: 3, brand: 'Lenovo', model: 'ThinkPad X1 Carbon' },
+]
+app.get('/LaptopsData',(req,res)=>{
+    res.send(LaptopData)
+})
+app.get('/', (req, res) => {
+    res.send(`Database connection status: ${isConnected() ? "Connection successful" : "Connection Unsuccessful"}`);
+});
+app.get('/ping', (req, res) => {
+    res.send("pong");
+});
+app.post('/Laptops', async(req,res)=>{
+    try{
+        const data = req.body
+        const new_product_data = new Products(data);
+        const response = await new_product_data.save()
+        console.log("data Saved");
+        res.status(200).json(response)
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal server Error"})
+    }
+})
+app.patch('/Laptops/:id', async(req,res)=>{
+    try{
+        const ID = req.params.id
+        const data = req.body
+        const Updated_data = await Products.findByIdAndUpdate(ID,data,{new:true})
+        const response = await Updated_data.save()
+        if(!Updated_data){
+            res.status(401).send("Data id not found")
+        }
+        res.send(Updated_data)
+        // return Updated_data
+    }catch(error){
+        res.status(500).send(error)
+    }
+})
+app.delete('/Laptops/:id', async(req,res)=>{
+    try{
+        const ID = req.params.id
+        const new_data = await Products.findByIdAndDelete(ID)
+        res.send(new_data)
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:"Internal Server Error"})
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
+
+
