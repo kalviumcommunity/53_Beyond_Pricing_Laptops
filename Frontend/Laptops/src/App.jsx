de99c7fa7b63ab2fa560bf9906c14b0825a218d2
HiMashuguptA <sg707012@gmail.com>
2024-02-09 16:13:53 +0530

    organised server

--- Frontend/Laptops/src/App.jsx
+++ Frontend/Laptops/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Linkings from '../routes/Routes'
+// import Fetchdata from '../Pages/Fetchdata'
+
 
 function App() {
 
   return (
     <>
       <Linkings/>
+      {/* <Fetchdata/> */}
+      
     </>
   )
 }
